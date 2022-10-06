2e330ec08834905c2c84cdccf9f96fd2e68fac10
Diego Leal <110353826+Diegoleal225@users.noreply.github.com>
2022-10-05 21:40:53 -0300

    Update calculadora.js

--- assets/scripts/calculadora.js
+++ assets/scripts/calculadora.js
@@ -1,80 +1,69 @@
+
 (function escopo(){
     (pageStatic=>{
-        const body= document.querySelector("body");
+        const body=document.querySelector("body");
         function recEvent(event){
             event.preventDefault();
         }
         body.addEventListener("submit",recEvent);
     })();
-    function calculadora(){
-        return{
-            display:document.querySelector(".result"),
-            get start(){
-                this.actionButton;
-                this.actionKeys;
-                this.display.focus();
-            },
-            get actionButton(){
-                document.addEventListener("click",(event)=>{
-                    const recEvent=event.target;
-                    if(recEvent.classList.contains("button-num")){
-                        this.showDisplay(recEvent.innerText);
-                    
-                    }else if(recEvent.classList.contains("button-x")){
-                        this.showDisplay("*");
-
-                    }else if(recEvent.classList.contains("button-ac")){
-                        this.clearAllDisplay;
-                    
-                    }else if(recEvent.classList.contains("button-c")){
-                        this.clearOne;
-                    
-                    }else if(recEvent.classList.contains("button")){
-                        this.calcular;
-                    }
-                })
-            },
-            get actionKeys(){
-                document.addEventListener("keydown",(event)=>{
-                    const recEvent=event.target; 
-                    if(event.key=="Backspace"){ 
-                        this.clearOne;
-                    }else if(event.key=="Enter"){
-                        this.calcular;
-                        this.display.focus();
-                    }   
-                })
-            },
-            showDisplay(valor){
-                this.display.value+=valor;
-            },
-            get clearAllDisplay(){
-                this.display.value=" ";
-            },
-            get clearOne(){
-                this.display.value=this.display.value.slice(0,-1);
-            },
-            get calcular(){
-                let calcular=this.display.value;
-                try{
-                    calcular=eval(calcular);
-                    if(!calcular){
-                        alert("Valores divergentes para operação");
-                        this.display.focus();
-                        this.clearAllDisplay;
-                        return;
-                    }
-                    this.display.value=String(calcular);
+    function Calculadora(){
+        this.start= ()=>{
+            this.display.focus();
+            this.actionClick();
+            this.actionKeys();
+        }
+        this.display=document.querySelector(".result");      
+        this.actionClick= ()=>{
+            document.addEventListener("click",(event)=>{
+                const recEvent=event.target;
+                if(recEvent.classList.contains("button-num")){
+                    this.showDisplay(recEvent.innerText);
+                
+                }else if(recEvent.classList.contains("button-x")){
+                    this.showDisplay("*");
+                }else if(recEvent.classList.contains("button-ac")){
+                    this.allClear();
                     this.display.focus();
-                }catch(e){
-                    alert("Valores divergentes para operação");
+                }else if(recEvent.classList.contains("button-c")){
+                    this.oneClear();
+                    
+                }else if(recEvent.classList.contains("button")){
+                    this.calculate();
                     this.display.focus();
-                    this.clearAllDisplay;
+                }
+            })
+        }
+        this.actionKeys=()=>{
+            document.addEventListener("keydown",(event)=>{
+                if(event.key=="Backspace"){
+                    this.oneClear();
+                }else if(event.key=="Enter"){
+                    this.calculate();
+                    this.display.focus();
+                }
+            })
+        }
+        this.showDisplay=(num)=> this.display.value+=num;
+        this.allClear=()=>this.display.value=" ";
+        this.oneClear=()=>this.display.value=this.display.value.slice(0,-1);       
+        this.calculate=()=>{
+            let valor=this.display.value;
+            try {
+                valor=eval(valor);
+                if(!valor){
+                    alert("Operação não disponivel para valores inseridos!");
+                    this.allClear();
                     return;
                 }
-                }              
+                this.display.value=String(valor);
+            } catch (error) {
+                alert("Operação não disponivel para valores inseridos!");
+                this.allClear();
+                return;
             }
-    };
-    const callCalculadora=calculadora();
-    callCalculadora.start;
+        }
+    }
+    const callCalculadora= new Calculadora();
+    callCalculadora.start();
 })();
